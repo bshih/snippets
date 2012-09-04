5bd74886dfae53b802ea75f0744b0b8bbbc4582f
Brian Shih <theclash@gmail.com>
2012-09-03 20:04:54 -0700

    refactor comment form, and show individual comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,4 +4,22 @@ class CommentsController < ApplicationController
     @comment = @post.comments.create(params[:comment])
     redirect_to(post_path(@post))
   end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to(post_path(@post))
+  end
+
+  def show
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @post }
+    end
+  end
+
 end
+
