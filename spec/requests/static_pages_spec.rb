868cad489438ea20048d18a9fe953056b1467081
Brian Shih <theclash@gmail.com>
2012-09-23 21:02:13 -0700

    refactor tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,39 +1,24 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  subject { page }
   describe "Home page" do
-    it "should have the content 'One-a-day'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit root_path
-      page.should have_content('One-a-day')
-    end
-    it "should have the right title" do
-      visit root_path
-      page.should_not have_selector('title', :text => " | Home")
-    end
+    before { visit root_path }
+    it { should have_selector('h1', text: 'One-a-day') }
+    it { should_not have_selector('title', text: ' | Home') }
 
   end
 
    describe "Help page" do
-    it "should have the content 'Help'" do
-      visit help_path
-      page.should have_content('Help')
-    end
-    it "should have the right title" do
-      visit help_path
-      page.should have_selector('title', :text => " | Help")
-    end
+    before { visit help_path }
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_selector('title', text: ' | Help') }
   end
 
   describe "About page" do
-    it "should have the content 'About'" do
-      visit about_path
-      page.should have_content('About')
-    end
-    it "should have the right title" do
-      visit about_path
-      page.should have_selector('title', :text => " | About")
-    end
+    before { visit about_path }
+    it { should have_selector('h1', text: 'About') }
+    it { should have_selector('title', text: ' | About') }
   end
 
 end
