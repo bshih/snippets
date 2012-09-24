086c32e075521bcaddf2e00895104bdc1efe9f28
Brian Shih <theclash@gmail.com>
2012-09-23 21:31:42 -0700

    refactor tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,23 +2,50 @@ require 'spec_helper'
 
 describe "StaticPages" do
   subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_selector('title', text: page_title) }
+  end
+
+
   describe "Home page" do
     before { visit root_path }
-    it { should have_selector('h1', text: 'One-a-day') }
-    it { should_not have_selector('title', text: ' | Home') }
+    let(:heading) { 'One-a-day' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
+
+
 
   end
 
    describe "Help page" do
     before { visit help_path }
-    it { should have_selector('h1', text: 'Help') }
-    it { should have_selector('title', text: ' | Help') }
+    let(:heading) { 'Help' }
+    let(:page_title) { ' | Help' }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
-    it { should have_selector('h1', text: 'About') }
-    it { should have_selector('title', text: ' | About') }
+    let(:heading) { 'About' }
+    let(:page_title) { ' | About' }
+
+    it_should_behave_like "all static pages"
   end
 
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: 'About'
+    click_link "Help"
+    page.should have_selector 'title', text: 'Help'
+    click_link "Home"
+    page.should have_selector 'title', text: ''
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: 'Sign Up'
+  end
 end
