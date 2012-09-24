3f356574b720fa38eb9286808fb369d1633636df
Brian Shih <theclash@gmail.com>
2012-09-23 20:54:00 -0700

    change to real routing, add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 OneADay::Application.routes.draw do
-  get "static_pages/home"
 
-  get "static_pages/help"
+  root to: "static_pages#home"
+  match '/help', to: 'static_pages#help'
+  match '/about', to: 'static_pages#about'
 
   resources :posts do
     resources :comments
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,26 +4,36 @@ describe "StaticPages" do
   describe "Home page" do
     it "should have the content 'One-a-day'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
+      visit root_path
       page.should have_content('One-a-day')
     end
     it "should have the right title" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text => " | Home")
+      visit root_path
+      page.should_not have_selector('title', :text => " | Home")
     end
 
   end
 
    describe "Help page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_content('Help')
     end
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title', :text => " | Help")
     end
   end
 
+  describe "About page" do
+    it "should have the content 'About'" do
+      visit about_path
+      page.should have_content('About')
+    end
+    it "should have the right title" do
+      visit about_path
+      page.should have_selector('title', :text => " | About")
+    end
+  end
 
 end
