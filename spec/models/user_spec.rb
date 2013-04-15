3090935dcf4dbf6b60e1d2051c18a700798f48f1
Brian Shih <theclash@gmail.com>
2013-04-14 22:38:33 -0700

    setting up user passwords

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,12 +12,15 @@
 require 'spec_helper'
 
 describe User do
-  before { @user = User.new(name: "Example User", email: "user@example.com") }
+  before { @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar") }
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
 
   it { should be_valid }
 
@@ -56,4 +59,36 @@ describe User do
       end      
     end
   end
+
+  describe "when email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+
+    it { should_not be_valid }
+  end
+
+  describe "when password is not present" do 
+    before do
+      @user.password = @user.password_confirmation = " "
+    end
+
+    it { should_not be_valid }
+
+  describe "when password does not match confirmation" do 
+    before do
+      @user.password = "foo"
+      @user.password_confirmation = "bar"
+    end
+
+    it { should_not be_valid }
+  end
+
+  describe "when password confirmation is nil" do
+    before { @user.password_confirmation = nil }
+    it { should_not be_valid }
+  end
+
 end
